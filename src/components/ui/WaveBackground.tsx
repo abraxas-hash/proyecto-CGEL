ff1b097f5fea5bcc59a83376d723072d67c084e0
abraxas-hash <217834301+abraxas-hash@users.noreply.github.com>
2026-06-13 16:42:11 -0500

    style: transform light mode background to hyper-vibrant 4K waves

--- src/components/ui/WaveBackground.tsx
+++ src/components/ui/WaveBackground.tsx
@@ -22,54 +22,54 @@ export default function WaveBackground() {
         </defs>
 
         {/* Background Base */}
-        <rect width="1920" height="1080" className="fill-[#e6e9ef] dark:fill-[#002b36] transition-colors duration-1000" />
+        <rect width="1920" height="1080" className="fill-[#0ea5e9] dark:fill-[#002b36] transition-colors duration-1000" />
 
-        {/* Layer 1 - Deep Teal (Dark) / Soft Silver (Light) */}
+        {/* Layer 1 - Blue/Teal */}
         <path 
           d="M0,0 L1920,0 L1920,300 C1400,500 1000,100 500,400 C200,550 0,300 0,300 Z" 
-          className="fill-[#e0e5ec] dark:fill-[#073642] transition-colors duration-1000"
+          className="fill-[#2563eb] dark:fill-[#073642] transition-colors duration-1000"
           filter="url(#drop-shadow)" 
         />
 
-        {/* Layer 2 - Dark Blue Wave (Dark) / Light Silver (Light) */}
+        {/* Layer 2 - Violet/Dark Blue */}
         <path 
           d="M0,200 C300,500 700,200 1100,500 C1500,800 1920,400 1920,400 L1920,1080 L0,1080 Z" 
-          className="fill-[#d8e0e8] dark:fill-[#268bd2] transition-colors duration-1000"
+          className="fill-[#7c3aed] dark:fill-[#268bd2] transition-colors duration-1000"
           filter="url(#drop-shadow)" 
         />
 
-        {/* Layer 3 - Purple Wave (Dark) / Neutral Silver (Light) */}
+        {/* Layer 3 - Fuchsia/Purple */}
         <path 
           d="M0,450 C400,300 800,800 1300,500 C1600,300 1920,600 1920,600 L1920,1080 L0,1080 Z" 
-          className="fill-[#d1dbe4] dark:fill-[#6c71c4] transition-colors duration-1000"
+          className="fill-[#d946ef] dark:fill-[#6c71c4] transition-colors duration-1000"
           filter="url(#drop-shadow-heavy)" 
         />
 
-        {/* Layer 4 - Pink Wave (Dark) / Ice Silver (Light) */}
+        {/* Layer 4 - Rose/Pink */}
         <path 
           d="M0,700 C300,900 600,500 1000,700 C1400,900 1700,600 1920,800 L1920,1080 L0,1080 Z" 
-          className="fill-[#c9d4de] dark:fill-[#d33682] transition-colors duration-1000"
+          className="fill-[#f43f5e] dark:fill-[#d33682] transition-colors duration-1000"
           filter="url(#drop-shadow-heavy)" 
         />
 
-        {/* Layer 5 - Orange Wave (Dark) / Steel Silver (Light) */}
+        {/* Layer 5 - Orange */}
         <path 
           d="M-100,1080 C200,800 600,1100 1000,800 C1400,500 1800,1000 2000,1080 Z" 
-          className="fill-[#c2cdd7] dark:fill-[#cb4b16] transition-colors duration-1000"
+          className="fill-[#f97316] dark:fill-[#cb4b16] transition-colors duration-1000"
           filter="url(#drop-shadow-heavy)" 
         />
 
-        {/* Layer 6 - Yellow Corner (Dark) / Deeper Silver (Light) */}
+        {/* Layer 6 - Amber/Yellow */}
         <path 
           d="M-50,1080 C150,950 400,1000 500,1080 Z" 
-          className="fill-[#bbc6d0] dark:fill-[#b58900] transition-colors duration-1000"
+          className="fill-[#f59e0b] dark:fill-[#b58900] transition-colors duration-1000"
           filter="url(#drop-shadow)" 
         />
         
-        {/* Layer 7 - Right Orange/Yellow Corner (Dark) / Accent Silver (Light) */}
+        {/* Layer 7 - Yellow Corner */}
         <path 
           d="M1400,1080 C1500,850 1800,800 2000,1080 Z" 
-          className="fill-[#b4bfca] dark:fill-[#ffb000] transition-colors duration-1000"
+          className="fill-[#eab308] dark:fill-[#ffb000] transition-colors duration-1000"
           filter="url(#drop-shadow)" 
         />
       </svg>
